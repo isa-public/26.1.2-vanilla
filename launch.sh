0ef76fe78d3f254a4e32fff3b2ec57b41ea93889
isa-public <heyimpr.t@gmail.com>
2026-05-24 14:29:27 +0000

    update launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+echo "Current timestamp: $timestamp" > ./times
 sudo tailscaled --tun=userspace-networking &
 sudo tailscale up --ssh &
 bash /workspaces/26.1.2-vanilla/server/server.sh
